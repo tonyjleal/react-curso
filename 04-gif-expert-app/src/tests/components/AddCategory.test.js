e1de2cb145175628aa5e7fcc33134d534a18ccaf
tonyjleal <tonyjleal@gmail.com>
2022-01-17 18:37:26 +0100

    test: simulate submit and clear inputValue

--- 04-gif-expert-app/src/tests/components/AddCategory.test.js
+++ 04-gif-expert-app/src/tests/components/AddCategory.test.js
@@ -26,19 +26,33 @@ describe('Pruebas en <AddCategory />', () => {
         const value = 'Hola Mundo';
 
         input.simulate('change', { target: { value } });
-
+        
         expect(wrapper.find('p').text().trim()).toBe( value );
-
+        
     }); 
     
     test('NO debe postear la información con submit', () => {
         wrapper.find('form').simulate('submit', { preventDefault(){} });
         
         // Comprobamos que no se llama al método setCategories al hacer submit
-        expect(setCategories ).not.toHaveBeenCalled();
-
+        expect(setCategories).not.toHaveBeenCalled();
+        
     });
     
+    test('debe de llamar el setCategories y limpiar la caja de texto', () => {
+        const value = 'Hola Mundo';
+
+        wrapper.find('input').simulate('change', { target: { value } });
+        wrapper.find('form').simulate('submit', { preventDefault(){} });
+
+        expect(setCategories).toHaveBeenCalled();
+        // Ha sido llamada 1 vez
+        expect(setCategories).toHaveBeenCalledTimes(1);
+        // Se haya llamado con una función y no con números o string...
+        expect(setCategories).toHaveBeenCalledWith(expect.any(Function));
+
+        expect(wrapper.find('input').prop('value').trim()).toBe('');
+    });
 
 });
 
