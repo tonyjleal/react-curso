5ab8322982423da704717c82b1e5c3f8544b4a91
tonyjleal <tonyjleal@gmail.com>
2022-01-20 14:09:40 +0100

    waitForNextUpdate

--- 04-gif-expert-app/src/tests/hooks/useFetchGifs.test.js
+++ 04-gif-expert-app/src/tests/hooks/useFetchGifs.test.js
@@ -3,14 +3,29 @@ import { renderHook } from '@testing-library/react-hooks';
 
 describe('Pruebas en hook useFetchGifs', () => {
     
-    test('debe retornar estado inicial ', () => {
+    test('debe retornar estado inicial ', async() => {
 
         // const { data, loading} = useFetchGifstchGifs('One Punch');
-        const { result } = renderHook(() => useFetchGifs('OnePunch'));
+        const { result, waitForNextUpdate } = renderHook(() => useFetchGifs('OnePunch'));
         const { data, loading } = result.current;
         
+        await waitForNextUpdate();
+
         expect( data ).toEqual([]);
         expect( loading ).toBe(true);
     });
     
+    test('debe retornar un arreglo de imgs y loading false ', async() => {
+       
+        const { result, waitForNextUpdate } = renderHook(() => useFetchGifs('OnePunch'));
+        await waitForNextUpdate();
+        const { data, loading } = result.current;
+        
+        expect( data.length ).toBe(10);
+        expect( loading ).toBe(false);
+
+
+    });
+
+
 });
