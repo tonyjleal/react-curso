b3af889128ddb21c7ac159d5f738076247638bb5
tonyjleal <tonyjleal@gmail.com>
2022-01-23 13:25:27 +0100

    useEffect - Cautions ;

--- 05-hook-app/src/components/02-useEffect/Message.js
+++ 05-hook-app/src/components/02-useEffect/Message.js
@@ -1,22 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 export const Message = () => {
 
-
+    const [coords, setCoords] = useState({x: 0, y:0})
+    const { x, y } = coords;
     useEffect(() => {
         
-        console.log('Componente montado');
-        
+        const mouseMove = (e) => {
+            const  coords = { x: e.x, y: e.y };
+            setCoords(coords);
+        }
+
+        window.addEventListener('mouseover', mouseMove);
+
         return () => {
-            console.log('Componente desmontado');
+            window.removeEventListener('mouseover', mouseMove);
         }
     }, [])
 
 
     return (
         <div>
-            <h3>Título h3</h3>
+            <h3>Coordenadas</h3>
+            <p>
+                x: {x} y : {y}
+            </p>
         </div>
     )
 }
