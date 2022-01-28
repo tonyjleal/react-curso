688a7a001173b5b444a381a8c41b979238cce2ab
tonyjleal <tonyjleal@gmail.com>
2022-01-28 13:05:03 +0100

    useFetch + useCounter

--- 05-hook-app/src/components/03-examples/MultipleCustomHooks.js
+++ 05-hook-app/src/components/03-examples/MultipleCustomHooks.js
@@ -1,14 +1,41 @@
 import React from 'react'
+import { useCounterMultiple } from '../../hooks/useCounterMultiple'
 import { useFetch } from '../../hooks/useFetch'
 import '../02-useEffect/effects.css'
 
 export const MultipleCustomHooks = () => {
 
-    const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
-    console.log(state);
+    const { counter, increment } = useCounterMultiple(1);
+
+    const {loading, data} = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+
+    const { author, quote } = !!data && data[0];
+
     return (
         <div>
-            <h1>Custom Hooks !</h1>
+            <h1>BreakingBad Quotes</h1>
+            <hr/>
+
+            {
+                loading 
+                ?
+                (                    
+                    <div className='alert alert-info text-center'>
+                        Loading...
+                    </div>
+                )
+                :
+                (
+                    <blockquote className='blockquote text-right'>
+                        <p className='mb-0'>{quote}</p>
+                        <footer className='blockquote-footer'>{author}</footer>
+                    </blockquote>
+                )
+            }
+
+            <button className='btn btn-primary' onClick={increment}>
+                Siguiente quote
+            </button>
         </div>
     )
 }
