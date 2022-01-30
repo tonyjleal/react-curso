fe1f27f4244426d8186d10edc6402e28701690ac
tonyjleal <tonyjleal@gmail.com>
2022-01-30 20:31:05 +0100

    useLayoutEffect

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -8,8 +8,9 @@ import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook
 import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 import { FocusScreen } from './components/04-useRef/FocusScreen';
 import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+import { LayoutEffect } from './components/05-useLayoutEffect/LayoutEffect';
 
 ReactDOM.render(
-    <RealExampleRef />,
+    <LayoutEffect />,
   document.getElementById('root')
 );
