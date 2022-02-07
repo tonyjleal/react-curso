c2256e16196341d0a31599c21d649dcd39ab132b
tonyjleal <tonyjleal@gmail.com>
2022-02-07 13:22:30 +0100

    style: Add html form for add todos

--- 05-hook-app/src/components/08-useReducer/TodoApp.js
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -19,11 +19,43 @@ export const TodoApp = () => {
 
         <h1> TODO APP ( { todos.length })</h1>
         <hr />
-        <ul>
-          <li>Hola</li>
-          <li>Mundo</li>
-          <li>Prueba</li>
-        </ul>
+        <div  className='row'
+        >
+          <div className='col-7'>
+            <ul className='list-group list-group-flush'>
+              {
+                todos.map((todo, i) => {
+                  return <li
+                          key={todo.id}
+                          className='list-group-item'>
+                          <p className='text-center'>{ i +1 }. { todo.desc }</p> 
+                            <button className='btn btn-danger'>
+                              Borrar
+                            </button>
+                          </li>
+                })
+              }
+            </ul>
+          </div>
+          <div className='col-5'>
+            <h4>Agregar TODO</h4>
+            <hr />
+
+            <form>
+              <input 
+              className='form-control'
+              type="text" 
+              name="description"
+              placeholder='Aprender...'
+              autoComplete='off' />
+
+              <button type='button' className='btn btn-outline-primary col-12 mt-1'>
+                Agregar
+              </button>
+            </form>
+          </div>
+
+        </div>
     </div>
     );
 };
