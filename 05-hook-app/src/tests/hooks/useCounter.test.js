9e09677ed27e18e55d3e2311d4673be0a79c473b
tonyjleal <tonyjleal@gmail.com>
2022-02-14 23:22:57 +0100

    test: CustomHook - useCounter default values

---
+++ 05-hook-app/src/tests/hooks/useCounter.test.js
@@ -0,0 +1,18 @@
+import { renderHook } from '@testing-library/react-hooks'
+import { useCounter } from '../../hooks/useCounter';
+
+describe('Tests useCounter', () => { 
+
+    test('should return default values', () => { 
+
+        
+        const { result } = renderHook( () => useCounter());
+        
+        expect(result.current.counter).toBe(10);
+        expect(typeof result.current.increment).toBe('function');
+        expect(typeof result.current.decrement).toBe('function');
+        expect(typeof result.current.reset).toBe('function');
+    })
+
+
+})
