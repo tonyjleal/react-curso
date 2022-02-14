421b1fa20d8949f930f0d2f3f92fc349c0a84362
tonyjleal <tonyjleal@gmail.com>
2022-02-14 18:29:54 +0100

    feat: CreateContext and useContext

--- 05-hook-app/src/components/09-useContext/HomePage.js
+++ 05-hook-app/src/components/09-useContext/HomePage.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
 
 export const HomePage = () => {
+
+  const userContext = useContext(UserContext);
+
+  console.log(userContext);
+
   return <div>
       <h1>Home Page</h1>
       <hr />
--- 05-hook-app/src/components/09-useContext/MainApp.js
+++ 05-hook-app/src/components/09-useContext/MainApp.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { AppRouter } from './AppRouter';
+import { UserContext } from './UserContext';
 
 export const MainApp = () => {
+
+    const user = {
+        id: 123,
+        name: 'Test',
+        email: 'test@test.com'
+    }
+
     return (
-        <AppRouter />
+        <UserContext.Provider value={ user }>
+            <AppRouter />
+        </UserContext.Provider>
     )
 };
