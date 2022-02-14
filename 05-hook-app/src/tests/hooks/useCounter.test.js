e5f95d862a9c04a9014df089b7fa9479af5e2222
tonyjleal <tonyjleal@gmail.com>
2022-02-14 23:33:20 +0100

    test: Execute customHook's functions inside of act

--- 05-hook-app/src/tests/hooks/useCounter.test.js
+++ 05-hook-app/src/tests/hooks/useCounter.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks'
+import { renderHook, act } from '@testing-library/react-hooks'
 import { useCounter } from '../../hooks/useCounter';
 
 describe('Tests useCounter', () => { 
@@ -13,6 +13,52 @@ describe('Tests useCounter', () => {
         expect(typeof result.current.decrement).toBe('function');
         expect(typeof result.current.reset).toBe('function');
     })
+
+    test('should return initialState 100', () => { 
+        const { result } = renderHook( () => useCounter(100));
+        
+        expect(result.current.counter).toBe(100);
+     })
+
+     test('should increment one value', () => { 
+        const { result } = renderHook( () => useCounter(100));
+        const { increment } = result.current;
+
+        act(() => {
+            increment();
+        });
+
+        const { counter } = result.current;
+        expect(counter).toBe(101);
+
+      })
+
+      test('should decrement one value', () => { 
+        const { result } = renderHook( () => useCounter(100));
+        const { decrement } = result.current;
+
+        act(() => {
+            decrement();
+        });
+
+        const { counter } = result.current;
+        expect(counter).toBe(99);
+
+      })
+
+      test('should reset value 100', () => { 
+        const { result } = renderHook( () => useCounter(100));
+        const { increment, reset } = result.current;
+
+        act(() => {
+            increment();
+            reset();
+        });
+
+        const { counter } = result.current;
+        expect(counter).toBe(100);
+
+      })
 
 
 })
