59b61950b1d12e629953b6bf734c19b793b90e78
tonyjleal <tonyjleal@gmail.com>
2022-02-17 13:21:56 +0100

    test: Tests component TodoAdd

--- 05-hook-app/src/tests/components/08-useReducer/TodoAdd.test.js
+++ 05-hook-app/src/tests/components/08-useReducer/TodoAdd.test.js
@@ -1,15 +1,53 @@
 import {shallow} from 'enzyme'
+import { TodoAdd } from '../../../components/08-useReducer/TodoAdd';
 
-describe('first', () => { 
+describe('Test <TodoAdd />', () => { 
     
-    test('should first', () => { 
-        
+    const handleAddTodo = jest.fn();
 
 
+    const wrapper = shallow(
+        <TodoAdd
+            handleAddTodo={handleAddTodo}
+        />
+    );
 
+    test('should show component success', () => { 
+            expect(wrapper).toMatchSnapshot();
      })
 
+     test('should doesnt call to handleAddTodo', () => { 
+        
+        const formSubmit = wrapper.find('form').prop('onSubmit');
 
+        formSubmit({preventDefault(){}});
 
+        expect(handleAddTodo).toHaveBeenCalledTimes(0);
+
+      })
+
+      test('should calls to function handleAddTodo', () => { 
+        
+        const value = 'Aprender React'
+        wrapper.find('input').simulate('change', {
+            target: {
+                value,
+                name: 'description'
+            }
+        })
+
+        const formSubmit = wrapper.find('form').prop('onSubmit');
+
+        formSubmit({preventDefault(){}});
+
+        expect(handleAddTodo).toHaveBeenCalledTimes(1);
+        expect(handleAddTodo).toHaveBeenCalledWith(expect.any(Object));
+        expect(handleAddTodo).toHaveBeenCalledWith({
+            id: expect.any(Number),
+            desc: value,
+            done: false,
+        });
+        expect(wrapper.find('input').prop('value')).toBe('');
+      })
 
  })
