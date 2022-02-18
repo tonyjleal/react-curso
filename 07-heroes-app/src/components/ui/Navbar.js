4adb1cd04acc8e12d619bc66d9e8d8941e088874
tonyjleal <tonyjleal@gmail.com>
2022-02-18 11:27:29 +0100

    feat: create second Router

--- 07-heroes-app/src/components/ui/Navbar.js
+++ 07-heroes-app/src/components/ui/Navbar.js
@@ -34,6 +34,12 @@ export const Navbar = () => {
                     >
                         DC
                     </NavLink>
+                    <NavLink 
+                        className={ ({ isActive }) => 'nav-item nav-link ' + ( isActive ? 'active' : '' ) } 
+                        to="/heroe"
+                    >
+                        Heroe
+                    </NavLink>
                 </div>
             </div>
 
--- 07-heroes-app/src/routers/AppRouter.js
+++ 07-heroes-app/src/routers/AppRouter.js
@@ -1,21 +1,15 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { DcScreen } from "../components/dc/DcScreen";
 import { LoginScreen } from "../components/login/LoginScreen";
-import { MarvelScreen } from "../components/marvel/MarvelScreen";
-import { SearchScreen } from "../components/search/SearchScreen";
-import { Navbar } from "../components/ui/Navbar";
+import { DashboardRouter } from "./DashboardRouter";
 
 export const AppRouter = () => {
   return (
     <div>
         <BrowserRouter>
-            <Navbar />
             <Routes>
-                <Route path="/" element={<MarvelScreen />} />
-                <Route path="/marvel" element={<MarvelScreen />} />
-                <Route path="/dc" element={<DcScreen />} />
-                <Route path="/searc" element={<SearchScreen />} />
-                <Route path="/login" element={<LoginScreen />} />
+              <Route path="/login" element={<LoginScreen />} />
+
+              <Route path="/*" element={ <DashboardRouter /> } />
             </Routes>
         </BrowserRouter>
     </div>
