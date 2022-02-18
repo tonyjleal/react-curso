a3cfb711db3be09ea79144486f1276cb998cbd5a
tonyjleal <tonyjleal@gmail.com>
2022-02-18 18:05:20 +0100

    perf: use useMemo when call method that get data

--- 07-heroes-app/src/components/hero/HeroScreen.js
+++ 07-heroes-app/src/components/hero/HeroScreen.js
@@ -1,21 +1,57 @@
-import { useParams, Navigate } from "react-router-dom"
+import { useMemo } from 'react'
+import { useParams, useNavigate, Navigate } from "react-router-dom"
 import { getHeroById } from "../../selectors/getHeroById";
-
 export const HeroScreen = () => {
 
-  const { id } = useParams();
+  
+  const { id: heroId } = useParams();
+  const navigate = useNavigate();
 
-  const hero = getHeroById(id);
+  const hero = useMemo(() => getHeroById(heroId), [heroId])
+
+  const handlerReturn = () => {
+    const [path,] = heroId.split('-');
+    return navigate(`/${path}`)
+  }
 
   if(!hero) return <Navigate to='/' />
 
-
+  const {
+    id,
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters
+  } = hero;
+  const imagePath = `/assets/${id}.jpg`
   return (
-    <div>
-        <h1>Hero Screen</h1>
-        <p>
-          { hero.superhero }
-        </p>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img 
+          src={imagePath}
+          alt={superhero}
+          className="img-thumbnail"
+        />
+      </div>
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"><b>Alter ego: </b> {alter_ego}</li>
+          <li className="list-group-item"><b>Publisher: </b> {publisher}</li>
+          <li className="list-group-item"><b>First Appearance </b> {first_appearance}</li>
+        </ul>
+
+        <h5 className="mt-3">Characters</h5>
+        <p>{characters}</p>
+
+        <button
+          className="btn btn-outline-info"
+          onClick={handlerReturn}
+        >
+          Regresar
+        </button>
+      </div>
     </div>
   )
 }
