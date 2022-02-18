e790b0df5fa9ee48f569a6cba89571ea31adc7a9
tonyjleal <tonyjleal@gmail.com>
2022-02-18 23:53:39 +0100

    feat: SearchComponent

--- 07-heroes-app/src/components/search/SearchScreen.js
+++ 07-heroes-app/src/components/search/SearchScreen.js
@@ -1,5 +1,48 @@
+import { useForm } from "../../hooks/useForm/useForm";
+
 export const SearchScreen = () => {
+
+  const [formValues, handleInputChange, reset] = useForm({
+    searchText: ''
+  });  
+
+  const {searchText} = formValues;
+
+  const handleSearch = (e) => {
+      e.preventDefault();
+      reset()
+      console.log(searchText);
+  }
+
   return (
-    <div><h1>Search Screen</h1></div>
+    <>
+      <h1>Búsquedas</h1>
+      <hr/>
+
+      <div className="row">
+        <div className="col-5">
+          <h4>Buscar</h4>
+          <hr/>
+          <form onSubmit={handleSearch}>
+            <input 
+              type="text"
+              placeholder="Buscar un héroe"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+              onChange={ handleInputChange }
+              value={searchText}
+            />
+
+            <button 
+              type="submit"
+              className="btn btn-primary mt-1"
+              >
+              Buscar...
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
